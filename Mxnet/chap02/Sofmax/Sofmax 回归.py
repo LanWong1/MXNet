7537ae0290815ee25f1964a5d201a9167041a600
IanWong <wangyi4422@163.com>
2018-12-07 16:38:21 +0800

    ss

---
+++ Mxnet/chap02/Sofmax/Sofmax 回归.py
@@ -0,0 +1,7 @@
+import gluonbook as gb
+from mxnet.gluon import data as gdata
+import sys
+import time
+
+mnist_train = gdata.vision.FashionMNIST(train=True)
+mnist_test = gdata.vision.FashionMNIST(train=False)
